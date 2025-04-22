284f0d47468d650e34a2e80fe6745cba5ccbfcef
DECKARDCAIM <cfallam2@miumg.edu.gt>
2025-04-22 06:03:38 -0600

    SE DESARROLLO TODO EL DASHBOARD INCLUYENDO LO SIGUIENTE, MENU DE PERFIL, MENU HAMBURGUESA, FOOTER, LOGOTIPO PARA DISPOSITIVOS, RESPONSIVO, SE USO CODIGO LIMPIO, AUN FALTAN CORREGIR ERRORES

---
+++ resources/views/includes/panel/menu.blade.php
@@ -0,0 +1,98 @@
+<hr class="horizontal dark" />
+<h6 class="ps-3 ms-2 text-uppercase text-xs font-weight-bolder text-dark">Panel de Controles</h6>
+
+<ul class="navbar-nav">
+    <!-- Emergencia -->
+    <li class="nav-item">
+        <a data-bs-toggle="collapse" href="#menuEmergencia" class="nav-link text-dark"
+            aria-controls="menuEmergencia" role="button" aria-expanded="false">
+            <i class="material-symbols-rounded opacity-5">space_dashboard</i>
+            <span class="nav-link-text ms-1 ps-1">Emergencia</span>
+        </a>
+        <div class="collapse" id="menuEmergencia">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="../../pages/dashboards/analytics.html">
+                        <span class="sidenav-normal ms-1 ps-1">Paciente</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="../../pages/dashboards/discover.html">
+                        <span class="sidenav-normal ms-1 ps-1">Lista de Turnos</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="../../pages/dashboards/discover.html">
+                        <span class="sidenav-normal ms-1 ps-1">Referencias</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </li>
+
+    <!-- Consulta Externa -->
+    <li class="nav-item">
+        <a data-bs-toggle="collapse" href="#menuConsulta" class="nav-link text-dark"
+            aria-controls="menuConsulta" role="button" aria-expanded="false">
+            <i class="material-symbols-rounded opacity-5">space_dashboard</i>
+            <span class="nav-link-text ms-1 ps-1">Consulta Externa</span>
+        </a>
+        <div class="collapse" id="menuConsulta">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="../../pages/dashboards/analytics.html">
+                        <span class="sidenav-normal ms-1 ps-1">Paciente</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="../../pages/dashboards/discover.html">
+                        <span class="sidenav-normal ms-1 ps-1">Lista de Turnos</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="../../pages/dashboards/discover.html">
+                        <span class="sidenav-normal ms-1 ps-1">Citas</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </li>
+
+    <!-- Mantenimiento -->
+    <li class="nav-item">
+        <a data-bs-toggle="collapse" href="#menuMantenimiento" class="nav-link text-dark"
+            aria-controls="menuMantenimiento" role="button" aria-expanded="false">
+            <i class="material-symbols-rounded opacity-5">space_dashboard</i>
+            <span class="nav-link-text ms-1 ps-1">Mantenimiento</span>
+        </a>
+        <div class="collapse" id="menuMantenimiento">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="../../pages/dashboards/analytics.html">
+                        <span class="sidenav-normal ms-1 ps-1">Doctores</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="../../pages/dashboards/discover.html">
+                        <span class="sidenav-normal ms-1 ps-1">Enfermeros</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="../../pages/dashboards/sales.html">
+                        <span class="sidenav-normal ms-1 ps-1">Especialidades</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="../../pages/dashboards/automotive.html">
+                        <span class="sidenav-normal ms-1 ps-1">Medicamentos</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-dark" href="../../pages/dashboards/smart-home.html">
+                        <span class="sidenav-normal ms-1 ps-1">Exámenes</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </li>
+</ul>
--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -2,28 +2,126 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('img/logo.png') }}">
     <title>{{ config('app.name') }} - @yield('tittle')</title>
     <link href="{{ asset('css/nucleo-icons.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/nucleo-svg.css') }}" rel="stylesheet" />
     <link id="pagestyle" href="{{ asset('css/material-dashboard.css?v=3.1.0') }}" rel="stylesheet" />
-    <link href="{{ asset('css/form.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css"
+        href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+
+
 </head>
 
-<body><form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <button type="submit" class="btn btn-danger btn-sm">
-        <i class="material-icons"></i> Cerrar sesión
-    </button>
-</form>
+<body class="g-sidenav-show  bg-gray-100">
 
-<script src="{{ asset('js/core/popper.min.js') }}"></script>
-<script src="{{ asset('js/core/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/plugins/perfect-scrollbar.min.js') }}"></script>
-<script src="{{ asset('js/plugins/smooth-scrollbar.min.js') }}"></script>
-<script src="{{ asset('js/material-dashboard.min.js?v=3.1.0') }}"></script>
+    <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-radius-lg fixed-start ms-2  bg-white my-2"
+        id="sidenav-main">
+        <div class="sidenav-header">
+            <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+                aria-hidden="true" id="iconSidenav"></i>
+            <a class="navbar-brand px-4 py-3 m-0">
+                <img src="{{ asset('img/logo.png')}}" class="navbar-brand-img" width="26" height="26"
+                    alt="main_logo">
+                <span class="ms-1 text-sm text-dark">HOSPROGRESO</span>
+            </a>
+        </div>
+        <hr class="horizontal dark mt-0 mb-2">
+        <div class="collapse navbar-collapse  w-auto h-auto" id="sidenav-collapse-main">
+            <ul class="navbar-nav">
+                <li class="nav-item mb-2 mt-0">
+                    <a data-bs-toggle="collapse" href="#ProfileNav" class="nav-link text-dark"
+                        aria-controls="ProfileNav" role="button" aria-expanded="false">
+                        <img src="{{ asset('img/foto-perfil.jpg')}}" class="avatar">
+                        <span class="nav-link-text ms-2 ps-1">Mi Cuenta</span>
+                    </a>
+                    <div class="collapse" id="ProfileNav" style="">
+                        <ul class="nav ">
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="../../pages/pages/profile/overview.html">
+                                    <span class="sidenav-mini-icon"></span>
+                                    <span class="sidenav-normal  ms-3  ps-1"> Mi Perfil </span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('formlogout').submit();">
+                                    <span class="sidenav-mini-icon"></span>
+                                    <span class="sidenav-normal ms-3 ps-1"> Cerrar Sesión </span>
+                                </a>
+                                <form method="POST" action="{{ route('logout') }}" style="display: none;" id="formlogout">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="collapse navbar-collapse  w-auto h-auto" id="sidenav-collapse-main">
+
+            @include('includes.panel.menu')
+
+        </div>
+    </aside>
+
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <nav class="navbar navbar-main navbar-expand-lg position-sticky mt-2 top-1 px-0 py-1 mx-3 shadow-none border-radius-lg z-index-sticky"
+            id="navbarBlur" data-scroll="true">
+            <div class="container-fluid py-1 px-2">
+                <div class="sidenav-toggler sidenav-toggler-inner d-xl-block d-none ">
+                    <a href="javascript:;" class="nav-link text-body p-0">
+                        <div class="sidenav-toggler-inner">
+                            <i class="sidenav-toggler-line"></i>
+                            <i class="sidenav-toggler-line"></i>
+                            <i class="sidenav-toggler-line"></i>
+                        </div>
+                    </a>
+                </div>
+                <nav aria-label="breadcrumb" class="ps-2">
+                    <ol class="breadcrumb bg-transparent mb-0 p-0">
+                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark"
+                                href="javascript:;">Inicio</a></li>
+                        <li class="breadcrumb-item text-sm text-dark active font-weight-bold" aria-current="page">
+                            Dashboard</li>
+                    </ol>
+                </nav>
+                <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+                    <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                        <div class="input-group input-group-outline">
+                        </div>
+                    </div>
+
+
+                    @include('includes.panel.userOptions')
+
+                </div>
+
+
+
+            </div>
+        </nav>
+
+        @yield('content')
+
+        @include('includes.panel.footer')
+
+    </main>
+
+    <script src="{{ asset('js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/smooth-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('js/material-dashboard.min.js?v=3.1.0') }}"></script>
 
 </body>
 
