305bde7ca69e9c4012620c93de2f41bdb367451d
DECKARDCAIM <cfallam2@miumg.edu.gt>
2025-04-23 12:31:30 -0600

    CORRIGIENDO ERROR EN LA RUTA

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect()->route('panel');
+    }
     return redirect()->route('login');
-});
+})->middleware('web'); // esto garantiza que funcione Auth::check()
 
 Auth::routes();
 
-Route::get('/panel', [App\Http\Controllers\HomeController::class, 'index'])->name('panel');
+Route::get('/panel', [App\Http\Controllers\HomeController::class, 'index'])->name('panel')->middleware('auth');
