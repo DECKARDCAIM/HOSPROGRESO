a5ecc12fb620b3fed32e19e7ed89d2a7f03bb518
DECKARDCAIM <cfallam2@miumg.edu.gt>
2025-06-26 22:00:07 -0600

    SE ELIMINARON MIGRACIONES INNECESARIAS E INTEGRADAS EN 1 SOLA MIGRACION

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('banner_photo_path', 2048)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
